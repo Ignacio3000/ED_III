ba5f08f0eeeb4ec1bb445f60828af0ff8d67bc8f
Ignacio3000 <ignacio.cechich@mi.unc.edu.ar>
2024-11-27 22:40:03 -0300

    Año2018Parcial2EJ 2

--- Ejercicios/Parcial_2_2018.c
+++ Ejercicios/Parcial_2_2018.c
@@ -1,6 +1,5 @@
 /*1.
-    Utilizando CMSIS escriba y comente un código que genere una onda del tipo trapezoidal a la salida del DAC como se
-muestra en la figura. Para ello el DAC comenzará, a partir de cero, a incrementar su valor de a un bits hasta llegar a un
+    Utilizando CMSIS escriba y comente un código que genere una onda del tipo trapezoidal a la salida del DAC. Para ello el DAC comenzará, a partir de cero, a incrementar su valor de a un bits hasta llegar a un
 valor máximo que se mantendrá un tiempo dado y luego decrementará de a un bits hasta volver a cero nuevamente. Los
 controles del valor máximo y los tiempos de duración de los escalones de subida y bajada están organizados en la
 posición de memoria 0x10004000 de la siguiente forma:
@@ -25,58 +24,104 @@ en bajada.
 #include "lpc17xx_timer.h"
 #include "lpc17xx_pinsel.h"
 
-#define MEMORIA_ADRESS 0x10004000 // Memory Adress
-#define valor_max ((0xFF) & ((uint32_t) * MEMORIA_ADRESS)) //Valor maximo a alcanzar por el DAC
-#define t_max ((0xFF << 8) & ((uint32_t) * MEMORIA_ADRESS)) //Tiempo del valor maximo
-#define t_alto ((0xFF << 16) & ((uint32_t) * MEMORIA_ADRESS)) //Tiempo en la subida
-#define t_bajo ((0xFF << 32) & ((uint32_t) * MEMORIA_ADRESS)) //Tiempo en la bajada
+#define MEMORIA_ADRESS ((uint32_t*)(0x10004000)) // Memory Adress
+
+#define valor_max (( *MEMORIA_ADRESS) & (0xFF) ) // Maximum DAC value
+
+#define t_max (( *MEMORIA_ADRESS >> 8) & (0xFF)) // Time at maximum value
+
+#define t_alto (( *MEMORIA_ADRESS >> 16) & (0xFF)) //Increment step delay
+
+#define t_bajo (( *MEMORIA_ADRESS >> 24) & (0xFF)) //Decrement step delay
+
 #define DMA_SIZE 10
 
+//Global Variables
+uint16_t VALOR_DAC; //DAC Value  
+uint16_t FLAG_BAJADA = 0; //Descending flag
 
-uint16_t VALOR_DAC = 0; //Inicializa el valor a cargar al DAC  
 
-
-
-
-void ConfPuertos(){
+//Fuction to configurate DAC pins
+void ConfPines(){
     PINSEL_CFG_Type Pincfg;
-    //Estructura que configura P0.26 como salida del DAC        
+           
     Pincfg.Pinnum = 26;
     Pincfg.Portnum = 0;
-    Pincfg.Pinmode = PINSEL_FUNC_2; //Func DAC
+    Pincfg.Pinmode = PINSEL_FUNC_2; //Set as DAC output
 
     PINSEL_ConfigPin(&Pincfg); //Cargo los parametros de la estructura
 }
 
 
-void Conf_DAC(){   
+void Conf_DAC(){  
+    VALOR_DAC = 0; //Inicializa valor a cargar a DAC 
     DAC_Init(LPC_DAC); //Inicializa DAC
 
+    
+
     DAC_CONVERTER_CFG_Type DAC_ConverCfg;
-    //Configuracion del DAC 
     DAC_ConverCfg.CNT_ENA = ENABLE; //Habilita time Out counter
     DAC_ConverCfg.DMA_ENA = ENABLE; //Hablita DMA
-    
     DAC_ConfigDAConverterControl(LPC_DAC , &DAC_ConverCfg);
+    
+    DAC_UpdateValue(LPC_DAC, VALOR_DAC);
     DAC_SetDMATimeOut(LPC_DAC , t_alto);
+
+    /*Habilitacion interrupcion DMA*/
+    NVIC_EnableIRQ(DMA_IRQn); 
 }
 
-void Conf_DMA(){
-    //HAGO QUE DMA INTERRUMPA ANTE CADA CONVERSION
-    GPDMA_Init(); //Primero inicializo DMA
-
-    GPDMA_Channel_CFG_Type DMAChannelCfg;
-
-    DMAChannelCfg.ChannelNum = 0;
-    DMAChannelCfg.TransferSize = DMA_SIZE; //10 bits del dac
-    DMAChannelCfg.TransferWidth = 0; //Usado solo cuando es de memomria a memoria
-    DMAChannelCfg.SrcMemAddr = ;
-    DMAChannelCfg.DstMemAddr =  
+int main(){
+    ConfPines();
+    Conf_DAC();
+    while (1)
+    {
+        /* code */
+    }
+    
 }
 
-void dma_handler(){
-    //Pregunto si llego al valor maximo
+void DMA_IRQHandler(){
+    if (FLAG_BAJADA)
+    {
+        
+        if (LPC_DAC -> DACCNTVAL != t_bajo)
+        {
+            DAC_SetDMATimeOut(LPC_DAC , t_bajo); //Carga bits 24 a 31
+        }
 
+        if(VALOR_DAC > 0)
+        {
+            VALOR_DAC--; //Decrementa el valor del DAC
+            DAC_UpdateValue(LPC_DAC, VALOR_DAC);
+        }
+        else if (VALOR_DAC == 0)
+        {
+            /* End of WAVEFORM generation */
+            NVIC_DisableIRQ(DMA_IRQn);
+            FLAG_BAJADA = 0;
+        }
+        /*else
+        {
+            GPDMA_ClearIntPending(GPDMA_STATCLR_INTTC , 0);
+        } */        
+    }
+    
+    else //Ascending or holding phase
+    {
+        if(VALOR_DAC < valor_max)
+        {
+            VALOR_DAC++;
+            DAC_UpdateValue(LPC_DAC, VALOR_DAC);//Update DAC
+        }
+        else if(VALOR_DAC == valor_max)
+        { //Hold at maximum
+            DAC_SetDMATimeOut(LPC_DAC, t_max); //Set maximum hold delay
+            FLAG_BAJADA = 1;     
+        }
+    }
+
+    GPDMA_ClearIntPending(GPDMA_STATCLR_INTERR, 0); //Clear DMA Interrupt 
 }
 
 
